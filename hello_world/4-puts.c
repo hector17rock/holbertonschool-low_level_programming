6f31a24f6e9934675596c9caf78f4346cd1e8b3d
Hector Soto <hector_17rock@hotmail.com>
2025-02-13 13:02:19 -0400

    4-puts.c1.4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*main - Entry Point
-*Return: 0 when done.
-*/
+ * main - Entry point
+ * Return: Return 0 when done.
+ */
 int main(void)
- {
-    puts("\"Programming is like building a multilingual puzzle,");
-    return (0);
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
