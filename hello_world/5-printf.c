cc6fc2480a9cf7d8748016963fb68fac4758f3d3
Hector Soto <hector_17rock@hotmail.com>
2025-02-13 13:04:19 -0400

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints "with proper grammar, but the outcome is a piece of art".
+ * Return: Return 0 when done.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
