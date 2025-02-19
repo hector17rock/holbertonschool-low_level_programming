f9416d89440015ecbc55388bd7615f38db34701e
Hector Soto <hector_17rock@hotmail.com>
2025-02-19 11:29:42 -0400

    0.putchartest2.9

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,25 +3,25 @@
 /* Function to check if a character is lowercase */
 int is_lowercase(char c)
 {
-    if (c >= 'a' && c <= 'z')
-        return 1;
-    else
-        return 0;
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
 
 /* Main function */
 int main(void)
 {
-    char ch = 'a';
+	char ch = 'a';
 
-    if (is_lowercase(ch))
-    {
-        printf("%c is lowercase.\n", ch);
-    }
-    else
-    {
-        printf("%c is not lowercase.\n", ch);
-    }
+	if (is_lowercase(ch))
+	{
+		printf("%c is lowercase.\n", ch);
+	}
+	else
+	{
+		printf("%c is not lowercase.\n", ch);
+	}
 
-    return 0;
+	return (0);
 }
