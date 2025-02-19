a5926de26e46de54102a693239f882eab53147d6
Hector Soto <hector_17rock@hotmail.com>
2025-02-19 10:16:44 -0400

    0.putchartest1.4

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 
-void _putchar(char c)
-{
-    putchar(c);
-}
-
 void print_alphabet(void)
 {
     char letter;
 
+    // Loop through the alphabet, from 'a' to 'z'
     for (letter = 'a'; letter <= 'z'; letter++)
     {
-        _putchar(letter); // Print each letter
+        putchar(letter);  // Print each letter
     }
-    
-    _putchar('\n'); // Print the newline after the alphabet
+
+    putchar('\n');  // Print the new line after the alphabet
 }
 
 int main(void)
 {
-    print_alphabet(); // Call the function to print the alphabet
+    print_alphabet();  // Call the function to print the alphabet
     return 0;
 }
-
