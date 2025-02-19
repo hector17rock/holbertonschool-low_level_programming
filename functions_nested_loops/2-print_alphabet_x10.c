96c88518aa557607b8a5bcf3a53752de95072378
Hector Soto <hector_17rock@hotmail.com>
2025-02-19 10:35:06 -0400

    0.putchartest1.8

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,28 +1,15 @@
-#include <unistd.h>
+#include <stdio.h>
 
-void _putchar(char c)
-{
-	write(1, &c, 1);
+void print_alphabet_ten_times() {
+    for (int i = 0; i < 10; i++) {
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            putchar(ch);  // Print each letter of the alphabet
+        }
+        putchar('\n');  // Print a new line after each alphabet
+    }
 }
 
-void print_alphabet(void)
-{
-	int i;
-	char letter;
-
-	// Repeat 10 times
-	for (i = 0; i < 10; i++)
-	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-		{
-			_putchar(letter);  // Print each letter
-		}
-		_putchar('\n');  // Print the new line after each iteration
-	}
-}
-
-int main(void)
-{
-	print_alphabet();  // Call the function to print the alphabet 10 times
-	return 0;
+int main() {
+    print_alphabet_ten_times();  // Call the function
+    return 0;
 }
