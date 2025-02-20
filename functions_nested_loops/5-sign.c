64369564ecd225488d43919bb0235e3a79845a0c
Hector Soto <hector_17rock@hotmail.com>
2025-02-20 10:13:55 -0400

    0.putchartest6.1

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,26 +1,29 @@
-#include <unistd.h>  /* For write() */
+#include "main.h"
 
 /**
- * print_sign - Prints the sign of a number.
- * @n: The number to check.
+ * print_sign - prints the sign of a number
+ * @n: the number to check
  *
- * Return: 1 if n is greater than 0, 0 if n is 0, -1 if n is less than 0.
+ * Return: 1 if n is greater than 0,
+ *         0 if n is zero,
+ *         -1 if n is less than 0
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		write(1, "+1", 2);  /* Write '+' and '1' together */
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		write(1, "0", 1);  /* Write '0' */
-		return (0);  /* No need to print the return value here */
+		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		write(1, "-1", 2);  /* Write '-' and '1' together */
+		_putchar('_');
 		return (-1);
 	}
 }
+
