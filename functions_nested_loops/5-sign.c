d2f183b4a8b9cee1a00d4438bb1bd531dd67e581
Hector Soto <hector_17rock@hotmail.com>
2025-02-20 09:13:28 -0400

    0.putchartest5.3

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_sign - Prints the sign of a number.
+ * @n: The number to check.
+ *
+ * Return: 1 if n is greater than 0, 0 if n is 0, -1 if n is less than 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+\n");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		printf("-\n");
+		return (-1);
+	}
+}
