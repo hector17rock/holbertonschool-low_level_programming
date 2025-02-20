bcaf57c14b3221f97278c3a9b05e94da01f136ae
Hector Soto <hector_17rock@hotmail.com>
2025-02-20 09:25:33 -0400

    0.putchartest5.8

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,8 +11,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		write(1, "+", 1);  /* Write '+' */
-		write(1, "1", 1);  /* Write return value '1' */
+		write(1, "+1", 2);  /* Write '+' and '1' */
 		return (1);
 	}
 	else if (n == 0)
@@ -23,8 +22,7 @@ int print_sign(int n)
 	}
 	else
 	{
-		write(1, "-", 1);  /* Write '-' */
-		write(1, "-1", 2);  /* Write return value '-1' */
+		write(1, "-1", 2);  /* Write '-' and '1' */
 		return (-1);
 	}
 }
