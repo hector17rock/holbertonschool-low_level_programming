d5b5b695d718259ac5c1b366f6fcf0a8ee39ad69
Hector Soto <hector_17rock@hotmail.com>
2025-02-20 08:49:58 -0400

    0.putchartest5

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,3 +1,9 @@
+/**
+ * _isalpha - Checks if the given character is an alphabetic letter.
+ * @c: The character to check.
+ * 
+ * Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise.
+ */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
