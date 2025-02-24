68e771b20e147a396c32eef463f8bc87811b1ec0
Hector Soto <hector_17rock@hotmail.com>
2025-02-24 14:03:32 -0400

    fixed warnings and errors in 4-print_most_numbers.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,33 @@
-abc
+#include "main.h"
+
+/**
+ * print_triangle - prints a right-aligned triangle of `#` characters.
+ * @size: the size of the triangle.
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');  /* Print a newline if size is 0 or negative */
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)  /* Loop through rows */
+		{
+			for (j = 1; j <= size - i; j++)  /* Print leading spaces */
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)  /* Print `#` characters */
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');  /* Newline after each row */
+		}
+	}
+}
+
