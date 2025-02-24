7e009ef1a02cdd863254e8a3e4e744b37877f81e
Hector Soto <hector_17rock@hotmail.com>
2025-02-24 12:53:34 -0400

    nested

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers from 0 to 9, except 4 and 6
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 4 && i != 6)
+		{
+			_putchar(i + '0');  /* Convert integer to character and print it */
+		}
+	}
+	_putchar('\n');  /* Print a newline after the numbers */
+}
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,3 +3,4 @@ int _isdigit(int c);
 int mul(int a, int b);
 int _putchar(char c);  /* Declare _putchar */
 void print_numbers(void);
+void print_most_numbers(void);
