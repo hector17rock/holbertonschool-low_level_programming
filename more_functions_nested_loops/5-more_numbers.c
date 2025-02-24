8577c9d1a68018cb0e3dfdec0d6dc5f8c9eae48d
Hector Soto <hector_17rock@hotmail.com>
2025-02-24 13:12:13 -0400

    fixed warnings and errors in 4-print_most_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14, excluding 2 and 4
+ *                10 times, each on a new line
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	/* Loop to print the sequence 10 times */
+	for (i = 0; i < 10; i++)
+	{
+		/* Loop for numbers from 0 to 14 */
+		for (j = 0; j <= 14; j++)
+		{
+			if (j != 2 && j != 4)
+			{
+				if (j >= 10)
+				{
+					_putchar((j / 10) + '0'); /* Print the tens digit */
+				}
+				_putchar((j % 10) + '0'); /* Print the ones digit */
+			}
+		}
+		_putchar('\n'); /* Print a newline after each iteration */
+	}
+}
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,3 +4,4 @@ int mul(int a, int b);
 int _putchar(char c);  /* Declare _putchar */
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
