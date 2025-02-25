11fccba971fc51ed03b0f260fe5d7442932c6394
Hector Soto <hector_17rock@hotmail.com>
2025-02-25 10:43:38 -0400

    pointers_arrays_strings

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of an interger to 98
+ *
+ * @n: pointer to the interger to be updated
+ */
+void reset_to_98(int *n)
+{
+	*n = 98; /* Deference the pointer and set the value to 98 */
+}
