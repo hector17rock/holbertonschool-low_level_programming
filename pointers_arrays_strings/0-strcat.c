64c5562c5da4d2d018e458a1dbdfcc0bd02d8fbf
Hector Soto <hector_17rock@hotmail.com>
2025-02-27 09:04:02 -0400

    fix: update 1-swap implementation1.2

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,16 @@
-char *_strcat(char *dest, const char *src)
+int main(void)
 {
-	char *dest_ptr = dest;
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+	char *ptr;
 
-	/* Move the pointer to the end of dest */
-	while (*dest_ptr != '\0') {
-		dest_ptr++;
-	}
+	printf("%s\n", s1);
+	printf("%s", s2);
+	ptr = _strcat(s1, s2);
+	printf("%s", s1);
+	printf("%s", s2);
+	printf("%s", ptr);
 
-	/* Copy src to the end of dest */
-	while (*src != '\0') {
-		*dest_ptr = *src;
-		dest_ptr++;
-		src++;
-	}
-
-	/* Null-terminate the concatenated string */
-	*dest_ptr = '\0';
-
-	return dest;
+	return (0);
 }
 
