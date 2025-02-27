33e19ed3c733c87c4c3b2b1530ac3b6d16c7e2e0
Hector Soto <hector_17rock@hotmail.com>
2025-02-27 09:54:39 -0400

    fix: update 1-swap implementation1.2

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -14,9 +14,7 @@ char *_strcat(char *dest, const char *src)
 
 	/* Move to the end of the destination string */
 	while (*dest_ptr != '\0')
-	{
 		dest_ptr++;
-	}
 
 	/* Append the source string to the destination string */
 	while (*src != '\0')
@@ -31,25 +29,3 @@ char *_strcat(char *dest, const char *src)
 
 	return (dest);
 }
-
-/**
- * main - Check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char s1[98] = "Hello ";
-	char s2[] = "World!\n";
-	char *ptr;
-
-	printf("%s\n", s1);
-	printf("%s", s2);
-	ptr = _strcat(s1, s2);
-	printf("%s", s1);
-	printf("%s", s2);
-	printf("%s", ptr);
-
-	return (0);
-}
-
