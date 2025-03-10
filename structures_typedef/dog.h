87061039b740c9a7f8060bfe19036c2fd45d6179
Hector Soto <hector_17rock@hotmail.com>
2025-03-10 13:50:16 -0400

    Add 0-create_array implementation

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a dog's attributes
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner's name
+ *
+ * Description: This struct stores information about a dog,
+ * including its name, age, and owner's name.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
