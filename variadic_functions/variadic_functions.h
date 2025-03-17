653f9b172eab48962bf017db7c35e3c240483dc9
Hector Soto <hector_17rock@hotmail.com>
2025-03-17 10:20:44 -0400

    Add 0-create_array implementation

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
 
