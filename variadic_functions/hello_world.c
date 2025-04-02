7d476a2a615bb424dcf7fd20022159dca1d47f7c
Hector Soto <hector_17rock@hotmail.com>
2025-04-02 11:41:22 -0400

    Commit changes

---
+++ variadic_functions/hello_world.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "Hello, World"
+ * followed by a new line using the printf function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Hello, World\n");
+	return (0);
+}
+
