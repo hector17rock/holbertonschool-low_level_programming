0dc0c845273ffbf3c9e99471c80f71a9480d283c
Hector Soto <hector_17rock@hotmail.com>
2025-04-02 13:14:24 -0400

    Add updated files

--- doubly_linked_lists/launch_test.sh
+++ doubly_linked_lists/launch_test.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 username="$1"
-echo "Running with username: $username"
+
+# Compile keygen5 if it's not already built
+gcc -Wall -Wextra -Werror -pedantic -std=gnu89 103-keygen.c -o keygen5 || exit 1
+
+# Generate key
 key=$(./keygen5 "$username")
+
+# Debug prints (optional, but useful)
+echo "Username: $username"
 echo "Generated key: $key"
 
-# Run the actual crackme
+# Run the actual test
 ./crackme5 "$username" "$key"
 
